24a80e4c7bab62ba9f6e370f49f454f613af8c9f
Jordan Patton <jordanpatton@gmail.com>
2019-04-13 13:55:12 -0700

    api routes for fixtures

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+const userFixtures = require('./fixtures/users.json');
+const widgetFixtures = require('./fixtures/widgets.json');
+
 const app = express();
 
 app.get('/', (_req, res, _next) => res.send('OK'));
 app.get('/api', (_req, res, _next) => res.json({}));
+app.get('/api/users', (_req, res, _next) => res.json({users: userFixtures}));
+app.get('/api/widgets', (_req, res, _next) => res.json({users: widgetFixtures}));
+app.get('*', (_req, res, _next) => res.status(404).send('Not Found'));
 
 app.listen(3000, () => console.log('listening on port 3000...'));
