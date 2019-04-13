124776b43046707cfb62eea59ca5113dd31e7d97
Jordan Patton <jordanpatton@gmail.com>
2019-04-13 13:45:07 -0700

    newlines

--- mobx-client/index.js
+++ mobx-client/index.js
@@ -1,2 +1,1 @@
 console.log('mobx client');
-
--- server/index.js
+++ server/index.js
@@ -6,4 +6,3 @@ app.get('/', (_req, res, _next) => res.send('OK'));
 app.get('/api', (_req, res, _next) => res.json({}));
 
 app.listen(3000, () => console.log('listening on port 3000...'));
-
