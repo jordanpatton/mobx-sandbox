990729145d631f3e7c7556fac2407f415fb76c13
Jordan Patton <jordanpatton@gmail.com>
2019-04-13 14:03:56 -0700

    GET routes

--- server/index.js
+++ server/index.js
@@ -7,8 +7,24 @@ const app = express();
 
 app.get('/', (_req, res, _next) => res.send('OK'));
 app.get('/api', (_req, res, _next) => res.json({}));
+
+// users
 app.get('/api/users', (_req, res, _next) => res.json({users: userFixtures}));
-app.get('/api/widgets', (_req, res, _next) => res.json({users: widgetFixtures}));
+app.get('/api/users/:userId', (req, res, _next) =>
+    res.json({users: userFixtures.filter(val =>
+        String(val.id) === String(req.params.userId)
+    )})
+);
+
+// widgets
+app.get('/api/widgets', (_req, res, _next) => res.json({widgets: widgetFixtures}));
+app.get('/api/widgets/:widgetId', (req, res, _next) =>
+    res.json({widgets: widgetFixtures.filter(val =>
+        String(val.id) === String(req.params.widgetId)
+    )})
+);
+
+// 404
 app.get('*', (_req, res, _next) => res.status(404).send('Not Found'));
 
 app.listen(3000, () => console.log('listening on port 3000...'));
