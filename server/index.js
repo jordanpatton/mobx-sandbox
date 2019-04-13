5e919e067afebc1f4d1620998303abcbcb59bb27
Jordan Patton <jordanpatton@gmail.com>
2019-04-13 13:23:24 -0700

    listener

--- server/index.js
+++ server/index.js
@@ -1,2 +1,9 @@
-console.log('server');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (_req, res, _next) => res.send('OK'));
+app.get('/api', (_req, res, _next) => res.json({}));
+
+app.listen(3000, () => console.log('listening on port 3000...'));
 
