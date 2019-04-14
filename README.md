7da6ef97e7ab9b3622aac5990804ec835a3bcb01
Jordan Patton <jordanpatton@gmail.com>
2019-04-13 18:43:38 -0700

    separate redux-app and add css reset

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ React State Management Sandbox
 
 ## Useful Links
 - https://www.valentinog.com/blog/react-webpack-babel/
+- https://meyerweb.com/eric/tools/css/reset/
--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App/index.jsx';
+export class Root extends React.Component {
+    constructor(...args) {
+        super(...args);
+        this.state = { selection: 'default' };
+    }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    renderHeader() {
+        return (
+            <div style={{ backgroundColor: '#CCCCCC' }}>
+                <button
+                    onClick={() => this.setState({ selection: 'default' })}
+                    type="button"
+                >
+                    default
+                </button>
+                <button
+                    onClick={() => this.setState({ selection: 'redux' })}
+                    type="button"
+                >
+                    redux
+                </button>
+            </div>
+        );
+    }
+
+    renderBody() {
+        switch (this.state.selection) {
+            case 'redux': return (<div>redux</div>); break;
+            default: return (<div>default</div>); break;
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderHeader()}
+                {this.renderBody()}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
