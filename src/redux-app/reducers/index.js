c7b43c666fb8b2bb9c664d69689bf55022bf967c
Jordan Patton <jordanpatton@gmail.com>
2019-04-13 19:00:30 -0700

    add widgets reducers

--- src/redux-app/reducers/index.js
+++ src/redux-app/reducers/index.js
@@ -4,6 +4,9 @@ import {
     INDEX_USERS_PENDING,
     INDEX_USERS_SUCCESS,
     INDEX_USERS_FAILURE,
+    INDEX_WIDGETS_PENDING,
+    INDEX_WIDGETS_SUCCESS,
+    INDEX_WIDGETS_FAILURE,
 } from '../actions/index.js';
 
 function users(state = {}, action) {
@@ -19,6 +22,20 @@ function users(state = {}, action) {
     }
 }
 
+function widgets(state = {}, action) {
+    switch (action.type) {
+        case INDEX_WIDGETS_PENDING:
+            return { ...state, error: undefined };
+        case INDEX_WIDGETS_SUCCESS:
+            return { ...state, data: action.data, error: undefined };
+        case INDEX_WIDGETS_FAILURE:
+            return { ...state, error: true };
+        default:
+            return state;
+    }
+}
+
 export default combineReducers({
     users,
+    widgets,
 });
