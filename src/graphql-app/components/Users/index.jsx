23df4eeb7e499ff5e474e6d34fed027dac7633b2
Jordan Patton <jordanpatton@gmail.com>
2019-04-14 23:05:59 -0700

    simpler arrow function

--- src/graphql-app/components/Users/index.jsx
+++ src/graphql-app/components/Users/index.jsx
@@ -16,8 +16,8 @@ class Users extends React.Component {
     render() {
         return (
             <Query query={GQL_INDEX_USERS}>
-                {({ loading, error, data }) => {
-                    return loading ? (
+                {({ loading, error, data }) => (
+                    loading ? (
                         <div style={{ padding: '24px' }}>Loading...</div>
                     ) : error ? (
                         <div style={{ padding: '24px' }}>Failed to fetch users.</div>
@@ -26,8 +26,8 @@ class Users extends React.Component {
                             <h2 style={{ fontSize: '36px' }}>All Users</h2>
                             <code>{JSON.stringify(data)}</code>
                         </div>
-                    );
-                }}
+                    )
+                )}
             </Query>
         );
     }
