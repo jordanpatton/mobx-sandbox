b489177357f48e75d03b8b3b94ae2f5f0f4b8f4c
Jordan Patton <jordanpatton@gmail.com>
2019-04-14 22:23:12 -0700

    simple query via apollo client

--- src/graphql-app/components/Users/index.jsx
+++ src/graphql-app/components/Users/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import gql from 'graphql-tag';
 
 import { AppContext } from '../App/context.js';
 
 class Users extends React.Component {
     render() {
         console.log(this.context);
+        this.context.apolloClient.query({
+            query: gql`
+            {
+                users {
+                    id
+                    first_name
+                    last_name
+                }
+            }
+            `
+        }).then(result => console.log(result));
         return (
             <div style={{ padding: '24px' }}>
                 Users
